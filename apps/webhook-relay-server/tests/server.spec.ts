2a2e01ded66640e4271f4e1c99a6b2e3b970eaf6
Daniel Soifer <daniel.soifer@codefresh.io>
2022-06-14 19:38:16 +0300

    added unit tests
    Signed-off-by: Daniel Soifer <daniel.soifer@codefresh.io>

--- apps/webhook-relay-server/tests/server.spec.ts
+++ apps/webhook-relay-server/tests/server.spec.ts
@@ -1,5 +1,7 @@
 import request from 'supertest'
 import EventSource from 'eventsource'
+import { LoggerService } from '@codefresh-io/logger'
+import { AUTH_TOKEN_HTTP_HEADER } from '@codefresh-io/common'
 
 import { Server } from '../src/server'
 import { createEventBus, EventBus } from '../src/eventbus'
@@ -10,52 +12,218 @@ const getNativeHttpServer = (serverWrap: Server): http.Server => {
     return (serverWrap as any).server // 'server' is a private field
 }
 
+interface EventSourceConfig {
+    channel: string
+    headers?: Record<string, string>
+}
+
+interface InitTestArgs {
+    eventSourceConfig: EventSourceConfig
+    eventbusConfig?: EventBusConfig
+    serverConfig?: ServerConfig
+    logger?: LoggerService
+}
+
+interface InitTestResolveValue {
+    eventbus: EventBus
+    server: Server
+    eventSource: EventSource
+}
+
+const defaultServerConfig: ServerConfig = {
+    port: 0, // allow the server to select an available port
+    heartbeatInterval: 100,
+}
+const defaultEventbusConfig: EventBusConfig = {
+    ...config.eventbus,
+}
+
+const initTest = async ({
+    eventSourceConfig,
+    eventbusConfig = defaultEventbusConfig,
+    serverConfig = defaultServerConfig,
+    logger = console,
+}: InitTestArgs): Promise<InitTestResolveValue> => {
+    // init eventbus
+    const eventbus = createEventBus(eventbusConfig, logger)
+    await eventbus.start()
+
+    // init server
+    const server = new Server(serverConfig, eventbus, logger)
+    const serverStartedPromise = new Promise(((resolve, reject) => {
+        server.start(() => resolve(null), reject)
+    }))
+    await serverStartedPromise
+
+    // init eventSource (client)
+    const { port } = getNativeHttpServer(server).address() as { port: number }
+    const eventSource = new EventSource(
+        `http://127.0.0.1:${port}/subscribe/${eventSourceConfig.channel}/`,
+        eventSourceConfig.headers && { headers: eventSourceConfig.headers },
+    )
+
+    return { eventbus, server, eventSource }
+}
+
 describe('Server', function () {
-    const serverConfig: ServerConfig = {
-        port: 0, // allow the server to select an available port
-        heartbeatInterval: 100,
-    }
-    const eventbusConfig: EventBusConfig = { ...config.eventbus }
-    const channel = 'fake-channel'
-    const logger = console
-    let server: Server
-    let eventbus: EventBus
-    let client: EventSource
+    describe('GET /subscribe/:channel', function () {
+        const channel = 'fake-channel'
+        let server: Server
+        let eventbus: EventBus
+        let client: EventSource
 
-    beforeEach(function (done) {
-        eventbus = createEventBus(eventbusConfig, logger)
-        eventbus.start()
-            .then(() => {
-                server = new Server(serverConfig, eventbus, logger)
-                server.start(
-                    () => {
-                        const { port } = getNativeHttpServer(server).address() as { port: number }
-                        client = new EventSource(`http://127.0.0.1:${port}/subscribe/${channel}/`)
+        afterEach(async function () {
+            await server.close()
+            eventbus.close()
+            client.close()
+        })
 
-                        // Wait for event source to be ready
-                        client.addEventListener('ready', () => done())
-                    },
-                    done
-                )
+        describe('given that the client subscribed to SSE successfully', function () {
+            beforeEach(async function () {
+                const result = await initTest({ eventSourceConfig: { channel } })
+                server = result.server
+                eventbus = result.eventbus
+                client = result.eventSource
+
+                const eventSourceReadyPromise = new Promise(resolve => {
+                    client.addEventListener('ready', () => resolve(null))
+                })
+                await eventSourceReadyPromise
             })
-            .catch(done)
-    })
 
-    afterEach(async function () {
-        await server.close()
-        eventbus.close()
-        client.close()
-    })
-
-    describe('GET /webhooks/:channel', function () {
-        describe('given that the client subscribed to SSE successfully (beforeEach)', function () {
-            it('should get heartbeat event', function (done) {
+            it('should get heartbeat from the server', function (done) {
                 client.addEventListener('heartbeat', () => done())
+            })
+        })
+
+        describe('given auth token', function () {
+            describe('when auth token is provided to both client and server', function () {
+                describe('when auth token is the same in both client and server', function () {
+                    const authToken = 'test'
+
+                    beforeEach(async function () {
+                        const result = await initTest({
+                            serverConfig: {
+                                ...defaultServerConfig,
+                                authToken,
+                            },
+                            eventSourceConfig: {
+                                channel,
+                                headers: { [AUTH_TOKEN_HTTP_HEADER]: authToken },
+                            },
+                        })
+                        server = result.server
+                        eventbus = result.eventbus
+                        client = result.eventSource
+                    })
+
+                    it('should get heartbeat from the server', function (done) {
+                        client.addEventListener('heartbeat', () => done())
+                    })
+                })
+
+                describe('when auth token in client is different from auth token in server', function () {
+                    const clientAuthToken = 'test1'
+                    const serverAuthToken = 'test2'
+
+                    beforeEach(async function () {
+                        const result = await initTest({
+                            serverConfig: {
+                                ...defaultServerConfig,
+                                authToken: serverAuthToken,
+                            },
+                            eventSourceConfig: {
+                                channel,
+                                headers: { [AUTH_TOKEN_HTTP_HEADER]: clientAuthToken },
+                            },
+                        })
+                        server = result.server
+                        eventbus = result.eventbus
+                        client = result.eventSource
+                    })
+
+                    it('should close connection with error', function (done) {
+                        client.addEventListener('error', (error: any) => {
+                            expect(error.status).toEqual(401)
+                            done()
+                        })
+                    })
+                })
+            })
+
+            describe('when auth token is provided only to the client', function () {
+                const clientAuthToken = 'test1'
+
+                beforeEach(async function () {
+                    const result = await initTest({
+                        eventSourceConfig: {
+                            channel,
+                            headers: { [AUTH_TOKEN_HTTP_HEADER]: clientAuthToken },
+                        },
+                    })
+                    server = result.server
+                    eventbus = result.eventbus
+                    client = result.eventSource
+                })
+
+                it('should get heartbeat from the server', function (done) {
+                    client.addEventListener('heartbeat', () => done())
+                })
+            })
+
+            describe('when auth token is provided only to the server', function () {
+                const serverAuthToken = 'test1'
+
+                beforeEach(async function () {
+                    const result = await initTest({
+                        serverConfig: {
+                            ...defaultServerConfig,
+                            authToken: serverAuthToken,
+                        },
+                        eventSourceConfig: {
+                            channel,
+                        },
+                    })
+                    server = result.server
+                    eventbus = result.eventbus
+                    client = result.eventSource
+                })
+
+                it('should close connection with error', function (done) {
+                    client.addEventListener('error', (error: any) => {
+                        expect(error.status).toEqual(401)
+                        done()
+                    })
+                })
             })
         })
     })
 
     describe('POST /webhooks/:channel', function () {
+        const channel = 'fake-channel'
+        let server: Server
+        let eventbus: EventBus
+        let client: EventSource
+
+        beforeEach(function (done) {
+            initTest({ eventSourceConfig: { channel } })
+                .then((initResult) => {
+                    server = initResult.server
+                    eventbus = initResult.eventbus
+                    client = initResult.eventSource
+
+                    // Wait for event source to be ready
+                    client.addEventListener('ready', () => done())
+                })
+                .catch(done)
+        })
+
+        afterEach(async function () {
+            await server.close()
+            eventbus.close()
+            client.close()
+        })
+
         describe('when payload is of json type', function () {
             it('should publish payload', function (done) {
                 const payload = { test: true }
